03cce8e9e98d7adc0c4ddd8e141a06d4aec60728
twan65 <cx.twan65@gmail.com>
2020-09-13 00:06:14 +0900

    add main package

---
+++ app.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+
+	"./src/google"
+)
+
+func main() {
+	cpuNumber := 3
+	runtime.GOMAXPROCS(cpuNumber)
+	var wait sync.WaitGroup
+	wait.Add(cpuNumber)
+
+	startTime := time.Now()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			google.Crawler()
+		}
+
+		defer wait.Done()
+	}()
+
+	wait.Wait()
+	elapsedTime := time.Since(startTime)
+
+	fmt.Printf("実行時間: %s\n", elapsedTime)
+}
