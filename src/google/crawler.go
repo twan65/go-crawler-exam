c843e8e188e177586bba0a506ffb6114ad8a6f7f
twan65 <cx.twan65@gmail.com>
2020-09-13 20:59:08 +0900

    コメント削除（DBは次回に）

--- src/google/crawler.go
+++ src/google/crawler.go
@@ -9,19 +9,6 @@ import (
 	"github.com/antchfx/htmlquery"
 )
 
-// func save() error {
-// 	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/test")
-
-// 	if err != nil {
-// 		fmt.Println("err")
-// 		return err
-// 	}
-
-// 	fmt.Println(db)
-
-// 	return nil
-// }
-
 func getHtml() (string, error) {
 	url := "http://www.google.com"
 	res, err := http.Get(url)
